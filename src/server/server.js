3ebde0fafdb52c038f9e7cc42b1c32438a6d8c09
jaker <jmcfly@gmail.com>
2018-05-26 21:28:24 -0700

    Server running on 8080, proxy set, app should send to port 8080.  Need to do some restructuring to where requests are sent.

--- src/server/server.js
+++ src/server/server.js
@@ -5,10 +5,10 @@ const port = 8080;
 const key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjOGM1YjNhMC0zZmY4LTAxMzYtNTZkMC0zMTI2NjQzMDhjNzEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTI2OTk4NjQ5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymctc3RhdHMtZTczOGUwOGMtMDRhYi00OTNiLWIwMjItNTZhYzA5ZTZhNTcwIiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MjV9.N83L9f6o00ERumw9IQqWJvWocBJA_JCYFQJQtQJc5aA";
 const url = "https://api.playbattlegrounds.com/shards";
 const authorization = "Bearer "+ key;
-const idRequestString = url+"/"+region+"/players/"+id;
-const matchRequestString = url+"/"+region+"/matches/"+matchID;
-const seasonsRequestString = url+"/"+region+"/seasons";
-const playerRequestString = url+"/"+region+"/players/"+id+"/seasons/"+seasonID;
+// const idRequestString = url+"/"+region+"/players/"+id;
+// const matchRequestString = url+"/"+region+"/matches/"+matchID;
+// const seasonsRequestString = url+"/"+region+"/seasons";
+// const playerRequestString = url+"/"+region+"/players/"+id+"/seasons/"+seasonID;
 const nameCache= {
 
 }
@@ -25,17 +25,17 @@ app.use(bodyParser.urlencoded({extended:false}));
 // Requests
 // Check for user ID on server
 app.get('/search/:name',function(req,res){
-  
+
 })
 
-app.get('/user/:id', function(req,res){
-    let userID = req.params.id;
+app.get('/user/', function(req,res){
+    console.log('hi');
 
 
 })
 
 // Cache Functions
-addNameToCache(name){
+function addNameToCache(name){
   if (name in nameCache){
 
   }
