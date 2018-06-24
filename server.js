c3af3a02c99faee9704a2d572fe6c56aeabc5a14
jaker <jmcfly@gmail.com>
2018-06-24 13:00:08 -0700

    Commit for heroku push.  Server now checks in the NODE_ENV is production before serving the static build folder.  Server now serves all get requests with * instead of /*. 6/24 1:00p

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ app.listen(port,function(){
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'/client/build')));
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static(path.join(__dirname,'/client/build')));
+}
+
 // cors
 // app.use(function(req,res,next){
 //   res.header("Access-Control-Allow-Origin","*");
@@ -38,8 +41,8 @@ app.use(function(err,req,res,next){
 
 // REQUESTS
 // Serve homepage on initial connection
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 
 // get recent searches
